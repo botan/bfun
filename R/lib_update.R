c16bea642b9d3307accde9776c8304b8063519ae
botan <aginbotan@gmail.com>
2021-10-16 20:49:51 +0100

    Add roxygen header to document lib_update()

--- R/lib_update.R
+++ R/lib_update.R
@@ -1,3 +1,21 @@
+#' Update All Installed Packages
+#'
+#' Update all installed packages with using \href{https://github.com/r-lib/pak}{pak}.
+#' Note: This function will be removed when \href{https://github.com/r-lib/pak}{pak}
+#' provides any equivalent.
+#'
+#' @param upgrade When `TRUE`, the default, will do ensure that you have the latest version of
+#'   `pkg` and all its dependencies.
+#'
+#'   When `upgrade = FALSE`, it will only upgrade packages if
+#'   them explicitly requires a higher version than what you currently have.
+#' @seealso \code{\link[utils]{update.packages}}
+#' @export
+#' @examples
+#' \dontrun{
+#' lib_update()
+#' lib_update(upgrade = FALSE)
+#' }
 lib_update <- function(upgrade = TRUE) {
   pkgs <- utils::old.packages()[, "Package"]
   if (!is.null(pkgs)) {
