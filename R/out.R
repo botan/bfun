4dbf660abc51164f6db9b57d590a20829097e5a3
botan <aginbotan@gmail.com>
2021-10-25 13:07:04 +0100

    Add roxygen header to document %out%

--- R/out.R
+++ R/out.R
@@ -1,1 +1,18 @@
+#' Find unmatched values
+#'
+#' %out% is an alternative way of using %in% for unmatched values,
+#' which provides more similarity with SQL syntax. Returns a logical vector
+#' indicating if there is an unmatch or not for its left operand.
+#'
+#' @param x A vector. Values to be unmatched.
+#' @param table A vector. Values to be unmatched against.
+#'
+#' @return A logical vector.
+#' @rdname out
+#' @export
+#'
+#' @seealso [match()]
+#' @examples
+#' 1:10 %out% 1:3
+#' letters %out% c("a", "e", "i", "o", "u")
 `%out%` <- function (x, table) match(x, table, nomatch = 0L) == 0L
