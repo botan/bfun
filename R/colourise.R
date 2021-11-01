a260308a9efeca6ededc89ee0385747fb7ad9b98
botan <aginbotan@gmail.com>
2021-11-01 10:46:47 +0000

    Add roxygen header to document colourise()

--- R/colourise.R
+++ R/colourise.R
@@ -1,3 +1,17 @@
+#' Generate HTML `<span>` element
+#'
+#' Vectorised over `text` and `colour`.
+#'
+#' @param text Either a character vector, or something coercible to one.
+#' @param colour A character vector.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' colourise("something should be red", "red")
+#' colourise(letters[1:3], "darkgray")
+#' colourise(letters[1:3], c("#ed2024", "#278e43", "#febd11"))
 colourise <- function(text, colour) {
   glue::glue('<span style="color: {colour};">{text}</span>')
 }
