eb0177a466401e99e92d7ec9779f442941d6c5ca
botan <aginbotan@gmail.com>
2022-02-16 20:22:02 +0000

    Add read_excel_all()

---
+++ R/read_excel_all.R
@@ -0,0 +1,9 @@
+read_excel_all <- function(path) {
+  sheet_names <- readxl::excel_sheets(path)
+  sheet_names_tidy <- snakecase::to_snake_case(sheet_names)
+
+  sheet_names %>%
+    purrr::map(readxl::read_excel, path = path) %>%
+    purrr::set_names(sheet_names_tidy) %>%
+    list2env(globalenv())
+}
