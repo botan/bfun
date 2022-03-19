2d473882d6277756f610544ff3395f63b7b9e677
botan <aginbotan@gmail.com>
2022-03-19 22:10:49 +0000

    Export last() and read_excel_all()

--- R/last.R
+++ R/last.R
@@ -1,3 +1,4 @@
+#' @export
 last <- function(x, na.rm = FALSE) {
   if (isTRUE(na.rm)) last <- na.omit(last)
   x[length(x)]
--- R/read_excel_all.R
+++ R/read_excel_all.R
@@ -1,3 +1,4 @@
+#' @export
 read_excel_all <- function(path) {
   sheet_names <- readxl::excel_sheets(path)
   sheet_names_tidy <- snakecase::to_snake_case(sheet_names)
