5a1a48d567003832ff32014747077032247cfe51
botan <aginbotan@gmail.com>
2022-03-19 22:48:00 +0000

    Use janitor::make_clean_names in read_excel_all()

--- R/read_excel_all.R
+++ R/read_excel_all.R
@@ -4,7 +4,9 @@ read_excel_all <- function(path) {
   sheet_names_tidy <- snakecase::to_snake_case(sheet_names)
 
   sheet_names %>%
-    purrr::map(readxl::read_excel, path = path) %>%
+    purrr::map(readxl::read_excel,
+               path = path,
+               .name_repair = janitor::make_clean_names) %>%
     purrr::set_names(sheet_names_tidy) %>%
     list2env(globalenv())
 }
