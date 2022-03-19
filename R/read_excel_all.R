6ff42ac0ca78aae2a249688bea0eea9bd6700e33
botan <aginbotan@gmail.com>
2022-03-19 23:15:48 +0000

    Add na parameter to read_excel_all()

--- R/read_excel_all.R
+++ R/read_excel_all.R
@@ -1,11 +1,12 @@
 #' @export
-read_excel_all <- function(path) {
+read_excel_all <- function(path, na = c("", "#N/A", "NULL")) {
   sheet_names <- readxl::excel_sheets(path)
   sheet_names_tidy <- snakecase::to_snake_case(sheet_names)
 
   sheet_names %>%
     purrr::map(readxl::read_excel,
                path = path,
+               na = na,
                .name_repair = janitor::make_clean_names) %>%
     purrr::set_names(sheet_names_tidy) %>%
     list2env(globalenv())
