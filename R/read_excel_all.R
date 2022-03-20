bbe5c9b95a138f3de5bd110b158f11af82400164
botan <aginbotan@gmail.com>
2022-03-20 00:38:34 +0000

    Set guess_max to Excel row limit

--- R/read_excel_all.R
+++ R/read_excel_all.R
@@ -1,5 +1,5 @@
 #' @export
-read_excel_all <- function(path, na = c("", "#N/A", "NULL")) {
+read_excel_all <- function(path, na = c("", "#N/A", "NULL"), guess_max = 1048526) {
   sheet_names <- readxl::excel_sheets(path)
   sheet_names_tidy <- snakecase::to_snake_case(sheet_names)
 
@@ -7,6 +7,7 @@ read_excel_all <- function(path, na = c("", "#N/A", "NULL")) {
     purrr::map(readxl::read_excel,
                path = path,
                na = na,
+               guess_max = guess_max,
                .name_repair = janitor::make_clean_names) %>%
     purrr::set_names(sheet_names_tidy) %>%
     list2env(globalenv())
