a080e424d288f1bfce62b63228cac43e85b85151
botan <aginbotan@gmail.com>
2022-06-01 19:52:13 +0100

    Fix bug tidy_postcode()

--- R/tidy_postcode.R
+++ R/tidy_postcode.R
@@ -3,5 +3,5 @@ tidy_postcode <- function(x) {
   x %>%
     stringr::str_to_upper() %>%
     stringr::str_remove_all(" ") %>%
-    stringr::str_replace_all("(.*)(.{3}$", "\\1 \\2")
+    stringr::str_replace_all("(.*)(.{3})$", "\\1 \\2")
 }
